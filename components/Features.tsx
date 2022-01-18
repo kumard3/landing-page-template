1ff774114df6a429cf4d8d73bd9cf8f61225190e
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-19 03:31:19 +0530

    service section added

--- components/Features.tsx
+++ components/Features.tsx
@@ -6,7 +6,7 @@ export default function Features() {
       <section className="p-4 lg:p-8 ">
         <div className="container mx-auto space-y-12">
           <div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row">
-            <div className="flex items-center justify-center flex-1 dark:bg-violet-400">
+            <div className="flex items-center justify-center flex-1 ">
               <img
                 src="/e-Commerce 2.png"
                 alt=""
@@ -28,7 +28,7 @@ export default function Features() {
             </div>
           </div>
           <div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row-reverse">
-            <div className="flex items-center justify-center flex-1 dark:bg-violet-400">
+            <div className="flex items-center justify-center flex-1 ">
               <img
                 src="/Finance 1.png"
                 alt=""
@@ -50,7 +50,7 @@ export default function Features() {
             </div>
           </div>
           <div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row">
-            <div className="flex items-center justify-center flex-1 dark:bg-violet-400">
+            <div className="flex items-center justify-center flex-1 ">
               <img
                 src="/Messages 1.png"
                 alt=""
@@ -73,7 +73,7 @@ export default function Features() {
           </div>
          
           <div className="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row-reverse">
-            <div className="flex items-center justify-center flex-1 dark:bg-violet-400">
+            <div className="flex items-center justify-center flex-1 ">
               <img
                src="/Service 1.png"
                 alt=""
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 /* eslint-disable @next/next/link-passhref */
 import Link from "next/link";
 import * as React from "react";
+import Card from "../components/Card";
 import Features from "../components/Features";
 import Hero from "../components/Hero";
 import NavComponent from "../components/NavComponent";
+import Services from "../components/Services";
 import Stats from "../components/Stats";
 
 export default function Home() {
@@ -13,6 +15,7 @@ export default function Home() {
       <Hero />
       <Stats />
       <Features />
+    <Services />
     </div>
   );
 }
