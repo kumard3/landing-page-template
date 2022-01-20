905ab76229cb795e2dd64c4e849675027abcf037
kumard3 <kumardeepanshu157200@gmail.com>
2022-01-20 12:50:42 +0530

    template 2 style

--- template2/components/Hero.tsx
+++ template2/components/Hero.tsx
@@ -1,21 +1,21 @@
 export default function Hero() {
   return (
     <div className="container w-full mx-auto flex flex-wrap justify-between mt-[2rem]">
-      <>
+    
 
       <div className="flex flex-col items-start justify-center max-w-xl  px-[30px]">
-        <h1 className="text-7xl font-bold  text-left">Let’s get fusely together</h1>
+        <h1 className="text-7xl font-bold  text-left">Let's get fusely together</h1>
         <p className="text-md">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus
           expedita id dolorum molestias delectus odio, incidunt est reiciendis
           laboriosam non quam dolor nam, ipsum similique quod modi nulla optio
           iure!
         </p>
-        <button className='p-3 bg-indigo-500 rounded-xl flex justify-start'> Get Started</button>
+        <button className='p-3 bg-indigo-500 rounded-xl flex justify-center sm:justify-start my-4'> Get Started</button>
       </div>
-      </>
-      <div className="flex flex-col items-center  justify-end">
-        <img src="/Hero image.svg" className="md:h-[30rem]"/>
+ 
+      <div className="flex flex-col items-center  justify-center">
+        <img src="/Hero image.svg" className="md:h-[30rem] p-10 md:p-0 "/>
       </div>
     </div>
   );
